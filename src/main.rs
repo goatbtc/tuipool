5d9205a3e322598c8a6f02032f7b8d8a8ee5f394
cirqueira-labs <jccirqueira@outlook.com>
2024-10-28 20:15:26 -0300

    moving to main

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ mod ui;
 
 use cursive::{Cursive, CursiveExt};
 use std::error::Error;
+use tokio::time;
 use ui::menubar::setup_menubar;
-use ui::blocks::*;
-use ui::exit::*;
-
+use ui::blocks::blocks_view;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -19,13 +18,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
     setup_menubar(&mut siv);
     siv.set_autohide_menu(false);
 
-    // Blocks view (initial load)
-    blocks_view(&mut siv);
-
+    // // Blocks view (initial load)
+    // blocks_view(&mut siv);
+    //
     // "q" to quit
-    siv.add_global_callback('q', |s| show_exit_dialog(s));
+    siv.add_global_callback('q', |s| s.quit());
 
+    // Start periodic block updates
+    let cb_sink = siv.cb_sink().clone();
+    tokio::spawn(async move {
+        loop {
+            // Wait for 5 seconds
+            time::sleep(time::Duration::from_secs(5)).await;
+
+            // Call blocks_view to update the UI with the latest blocks
+            let cb_sink = cb_sink.clone();
+            let _ = cb_sink.send(Box::new(move |s| {
+                ui::blocks::blocks_view(s);
+            }));
+        }
+    });
+
+    // Run the Cursive event loop
     siv.run();
 
     Ok(())
 }
+
